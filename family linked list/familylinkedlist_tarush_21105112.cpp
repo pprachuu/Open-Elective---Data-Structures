aa980295a69961a82e74c878457f9eadd4854b6a
Tarush <96109309+pprachuu@users.noreply.github.com>
2022-09-25 23:46:10 +0530

    Update familylinkedlist_tarush_21105112.cpp

--- family linked list/familylinkedlist_tarush_21105112.cpp
+++ family linked list/familylinkedlist_tarush_21105112.cpp
@@ -112,4 +112,9 @@ int main(){
     display_from_start(head);
 }
 
+/*Bonus Question
+Q.Try to find a way to link the family members' doubly-linked list based on their relationship.
 
+Ans.By sorting the doubly linked list according to age in decending order.
+    By doing so we will get a doubly linked list in which older generation people will be close to head in double linked list and new generation people will be close to tail in doubly linked list.
+*/
